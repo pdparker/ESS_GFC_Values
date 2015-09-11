cd15fa3ca78e2b32f62e47bb9cc9612b4f892ee9
Philip Parker <philip.parker@acu.edu.au>
2015-09-11 15:34:41 +1000

    added an example sem model.

---
+++ SEM_check.R
@@ -0,0 +1,29 @@
+#Load libraries
+library(lavaan)
+#set working directory
+setwd( "~/ess/2008/" )
+#load integrated data
+load("integrated.rda")
+#provide sample design
+#this is not really ideal but given not every country has a design matrix
+	# this will have to do for now
+ess4.design <- 
+	svydesign(
+		ids = ~idno,
+		weights = ~dweight, 
+		data = x
+	)
+
+modelSyntax <- " 
+Universalism =~ ipeqopt + ipudrst + impenv
+Benevolence  =~ iphlppl + iplylfr
+
+Tradition    =~ ipmodst + imptrad
+Conformity   =~ ipfrule + ipbhprp 
+Security     =~ impsafe + ipstrgv
+"
+
+Mtotal <- cfa(modelSyntax, data = x, group = "cntry") 
+Mtotal.survey <- lavaan.survey( Mtotal , ess4.design )
+fitmeasures(Mtotal.survey)
+standardizedsolution(Mtotal.survey)
